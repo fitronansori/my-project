11ca258d83c7825b080779dfb5c49e5f5a6d8cec
Fitron Ansori <fitronansori7@gmail.com>
2023-11-02 16:12:51 +0800

    fixed button comp

--- mern-blog-app/front-end/src/components/Button.jsx
+++ mern-blog-app/front-end/src/components/Button.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 
-const Button = ({ label, className }) => {
-  return <button className={className}>{label}</button>;
+const Button = ({ label, className, type }) => {
+  return (
+    <button className={className} type={type}>
+      {label}
+    </button>
+  );
 };
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
   className: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Button;
