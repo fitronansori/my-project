f291b766d13efede4fd80e59e661bf37ef5e34d1
Fitron Ansori <fitronansori7@gmail.com>
2023-11-07 15:25:06 +0800

    make listing creation step 4

--- next-airbnb-app/src/components/modals/RentModal.tsx
+++ next-airbnb-app/src/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,11 +47,12 @@ const RentModal = () => {
     },
   });
 
-  const category = watch("category");
   const location = watch("location");
+  const category = watch("category");
   const guestCount = watch("guestCount");
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
+  const imageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () =>
@@ -160,6 +162,22 @@ const RentModal = () => {
           subtitle="How many Bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a Photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+
+        <ImageUpload
+          onChange={(value) => setCustomValue("imageSrc", value)}
+          value={imageSrc}
         />
       </div>
     );
