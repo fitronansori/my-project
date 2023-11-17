d7b687a35957e4937653e80e0d6d95ae107a61fd
Fitron Ansori <fitronansori7@gmail.com>
2023-11-17 19:53:24 +0800

    make properties component UI

--- next-airbnb-app/src/app/actions/getListings.ts
+++ next-airbnb-app/src/app/actions/getListings.ts
@@ -1,7 +1,19 @@
 import prisma from "@/libs/prismadb";
 
-export default async function getListings() {
+export type ILisitngParams = {
+  userId?: string;
+};
+
+export default async function getListings(params: ILisitngParams) {
   try {
+    const { userId } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
     const listings = await prisma.listing.findMany({
       orderBy: {
         createdAt: "desc",
