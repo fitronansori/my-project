d74e9b1b57de7be39e733240a532f12c46d0749c
fitronansori <fitronansori7@gmail.com>
2023-12-18 22:33:02 +0800

    commit

---
+++ next-ecommerce/ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className="m-5">
+      <Button size={"default"} variant={"default"}>
+        Click me
+      </Button>
+    </div>
+  )
+}
+export default Home
