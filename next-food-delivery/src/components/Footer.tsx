04dc7b267c5e9e36e10253c6c13c19da0d6c351f
fitronansori <fitronansori7@gmail.com>
2024-01-02 08:35:10 +0800

    add menu ui

--- next-food-delivery/src/components/Footer.tsx
+++ next-food-delivery/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+    <div className="h-12 md:h-24 p-4 lg:p-20 text-red-500 flex items-center justify-between border-t-2 border-red-500">
       <Link href="/" className="font-bold text-xl">
         MASSIMO
       </Link>
