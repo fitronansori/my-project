c8a90414c3a78a7e37f9469d7a6906e01ea7777a
fitronansori <fitronansori7@gmail.com>
2024-01-03 09:27:05 +0800

    fixed and make video component ui

--- react-petworld-landing-page/src/App.tsx
+++ react-petworld-landing-page/src/App.tsx
@@ -1,23 +1,34 @@
 import Featured from "./components/featured/Featured";
 import Hero from "./components/hero/Hero";
 import Logos from "./components/logos/Logos";
+import Video from "./components/video/Video";
 
 const App = () => {
   return (
     <div className="w-full">
       {/* Hero Section */}
-      <section className="h-[calc(100vh-4rem)] md:h-[71vh] bg-gray-200">
+      <section className="h-[calc(100vh-4rem)] bg-gray-100">
         <Hero />
       </section>
       {/* End Hero Section */}
 
+      {/* Videos Section */}
+      <section className="lg:h-[60vh] bg-gray-100 flex justify-center items-center ">
+        <Video />
+      </section>
+      {/* End Videos Section */}
+
+      {/* Logos Section */}
       <section className="h-[160px] md:h-[200px]">
         <Logos />
       </section>
+      {/* End Logos Section */}
 
+      {/* Featured Section */}
       <section className="lg:h-[500px] flex justify-center items-center">
         <Featured />
       </section>
+      {/* End Featured Section */}
     </div>
   );
 };
