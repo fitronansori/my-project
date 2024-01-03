5d550b3c9cd916278408cbb49ab384bbaf55cf0f
fitronansori <fitronansori7@gmail.com>
2024-01-03 10:19:29 +0800

    make review contacthero section ui

--- react-petworld-landing-page/src/main.tsx
+++ react-petworld-landing-page/src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Layout from "./layout/Layout.tsx";
+// import Layout from "./layout/Layout.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    {/* <Layout> */}
+    <App />
+    {/* </Layout> */}
   </React.StrictMode>
 );
