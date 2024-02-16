d77c248f5d88088dee34df0468ab65ffc8d05021
fitronansori <fitronansori7@gmail.com>
2024-02-16 23:02:22 +0800

    Add login and signup functionality to frontend

--- mern-medicare-booking-app/frontend/src/pages/Login.jsx
+++ mern-medicare-booking-app/frontend/src/pages/Login.jsx
@@ -1,4 +1,82 @@
+import { useState } from "react";
+import Header from "../components/Header/Header";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <Header />
+
+      <section className="px-5 lg:px-0">
+        <div className="w-full max-w-[570px] mx-auto rounded-md md:shadow-lg md:p-10">
+          <h2 className="text-[20px] leading-9 text-headingColor font-bold mb-10">
+            Hello there! <span className="text-primaryColor">Welcome</span>
+          </h2>
+
+          <form action="" className="py-4 md:py-0">
+            <div className="mb-5">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter Your Email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus-within:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                required
+              />
+            </div>
+
+            <div className="mb-5">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter Your Password"
+                value={formData.password}
+                onChange={handleInputChange}
+                className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus-within:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                required
+              />
+            </div>
+
+            <div className="mt-7">
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="w-full bg-primaryColor text-white text-[22px] leading-[30px] rounded-lg px-4 py-3"
+              >
+                Login
+              </button>
+            </div>
+
+            <p className="mt-5 text-textColor text-center">
+              Don&apos;t have an account?
+              <Link
+                to={"/register"}
+                className="text-primaryColor font-medium ml-1"
+              >
+                Register
+              </Link>
+            </p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 export default Login;
--- mern-medicare-booking-app/frontend/src/pages/SignUp.jsx
+++ mern-medicare-booking-app/frontend/src/pages/SignUp.jsx
@@ -1,4 +1,30 @@
+import Header from "../components/Header/Header";
+
+// File
+import signUpImg from "../assets/images/signup.gif";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-5 lg:py-10 xl:px-0">
+        <div className="max-w-[1170px] mx-auto">
+          <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div className="hidden lg:block bg-primaryColor rounded-l-lg">
+              <figure className="rounded-l-lg">
+                <img src={signUpImg} alt="" className="w-full rounded-l-lg" />
+              </figure>
+            </div>
+
+            <div className="rounded-l-lg lg:pl-16 py-10 shadow-lg">
+              <h2 className="text-[20px] leading-9 text-headingColor font-bold mb-10">
+                Create an <span className="text-primaryColor">account</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default SignUp;
