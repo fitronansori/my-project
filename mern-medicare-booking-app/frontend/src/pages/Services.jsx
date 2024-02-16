20edbfab67d3afe76731df450f4fcba155b6e92c
fitronansori <fitronansori7@gmail.com>
2024-02-16 14:29:19 +0800

    feat : Add service cards to Services page

--- mern-medicare-booking-app/frontend/src/pages/Services.jsx
+++ mern-medicare-booking-app/frontend/src/pages/Services.jsx
@@ -1,4 +1,17 @@
+import { services } from "../assets/data/services";
+import ServiceCard from "../components/Services/ServiceCard";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section>
+      <div className="contaier">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-[30px]">
+          {services.map((item, index) => (
+            <ServiceCard key={index} item={item} index={index} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Services;
