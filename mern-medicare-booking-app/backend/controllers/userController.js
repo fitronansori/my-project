4de5ffb02417287919f13f4973076426bdac968f
fitronansori <fitronansori7@gmail.com>
2024-02-19 21:25:54 +0800

    Refactor userController.js

--- mern-medicare-booking-app/backend/controllers/userController.js
+++ mern-medicare-booking-app/backend/controllers/userController.js
@@ -1,64 +1,58 @@
 import User from "../models/UserSchema.js";
 
 const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find().select("-password");
+  try {
+    const users = await User.find().select("-password");
 
-        res.status(200).json({ message: "All users", data: users });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Internal server error, Try again" });
-    }
+    res.status(200).json({ message: "All users", data: users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error, Try again" });
+  }
 };
 
 const getSingleUser = async (req, res) => {
+  try {
     const { id } = req.params;
 
-    try {
-        const user = await User.findById(id).select("-password");
+    const user = await User.findById(id).select("-password");
 
-        res.status(200).json({ message: "User found", data: user });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: `User not found with id ${id}` });
-    }
+    res.status(200).json({ message: "User found", data: user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: `User not found with id ${id}` });
+  }
 };
 
 const updateUser = async (req, res) => {
+  try {
     const { id } = req.params;
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate(id, req.body, {
-            new: true,
-            runValidators: true,
-        });
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-        if (!updatedUser) {
-            return res.status(404).json({ message: `User not found with id ${id}` });
-        }
-
-        res.status(200).json({message: "Successfully updated", data: updatedUser });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Failed to update" });
-    }
+    res
+      .status(200)
+      .json({ message: "Successfully updated", data: updatedUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to update" });
+  }
 };
 
 const deleteUser = async (req, res) => {
+  try {
     const { id } = req.params;
 
-    try {
-        await User.findByIdAndDelete(id);
+    await User.findByIdAndDelete(id);
 
-        res.status(200).json({ message: "Successfully deleted" });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Failed to delete" });
-    }
+    res.status(200).json({ message: "Successfully deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to delete" });
+  }
 };
 
-export {getAllUsers, getSingleUser, updateUser, deleteUser };
+export { getAllUsers, getSingleUser, updateUser, deleteUser };
