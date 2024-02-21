65d1b0a12bbb08708bd2ee9412536982107fbca1
fitronansori <fitronansori7@gmail.com>
2024-02-21 20:39:02 +0800

    Add ProtectedRoute

--- mern-medicare-booking-app/frontend/src/router/ProtectedRoute.jsx
+++ mern-medicare-booking-app/frontend/src/router/ProtectedRoute.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { isLoggedIn } = useSelector((state) => state.auth);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (isLoggedIn) {
       navigate("/", { replace: true });
     }
+
+    setLoading(true);
   }, [isLoggedIn, navigate]);
 
-  return children;
+  return loading ? children : <div className="bg-white"></div>;
 };
 export default ProtectedRoute;
