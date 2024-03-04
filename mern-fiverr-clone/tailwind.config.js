753297c38bcc8bd016febdd4ef3d70f9ca47a057
fitronansori <fitronansori7@gmail.com>
2024-03-04 12:16:56 +0800

    Update tailwind.config.js and package.json

--- mern-fiverr-clone/tailwind.config.js
+++ mern-fiverr-clone/tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        "2xl": "2rem",
+      },
       screens: {
         "2xl": "1400px",
       },
