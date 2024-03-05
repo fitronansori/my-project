7a7554300bbd2db5d563ff324023722fbb33935f
fitronansori <fitronansori7@gmail.com>
2024-03-05 16:22:11 +0800

    Add GigCard and GigsSearch components to Gigs page

--- mern-fiverr-clone/src/pages/Gigs.tsx
+++ mern-fiverr-clone/src/pages/Gigs.tsx
@@ -1,4 +1,22 @@
+import GigCard from "@/components/gigs/GigCard";
+import GigsSearch from "@/components/gigs/GigsSearch";
+
 const Gigs = () => {
-  return <div>Gigs</div>;
+  return (
+    <section className="py-8">
+      <div className="container space-y-4">
+        <div className="space-y-2">
+          <h1 className="text-2xl font-bold">AI Artist</h1>
+          <p className="text-gray-600 font-light">
+            Explore the boundaries of art and technology with Fiverr AI artist.
+          </p>
+        </div>
+
+        <GigsSearch />
+
+        <GigCard />
+      </div>
+    </section>
+  );
 };
 export default Gigs;
